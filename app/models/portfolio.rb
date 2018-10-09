de1e1b0887494a395efe3f2298c421b675bfa0be
Anhad Gill <anhadgill@Anhads-MBP-2.lan>
2018-10-09 15:31:43 -0700

    Integrated concerns in models to load defaults for photos.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,6 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
-
+    include Placeholder
     def self.ruby_on_rails
         where(subtitle: 'Angular')
     end
@@ -12,7 +12,7 @@ class Portfolio < ApplicationRecord
     
 
     def set_defaults
-        self.main_image ||= "https://via.placeholder.com/600x400"
-        self.thumb_image ||= "https://via.placeholder.com/350x200"
+        self.main_image ||= Placeholder.image_generator(height: '600', width: '400') 
+        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
     end
 end
